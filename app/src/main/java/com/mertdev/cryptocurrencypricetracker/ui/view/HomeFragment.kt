3fbc0d765eb1dfd0cb4f15871951f4106fa43c3a
mrtunluer <mertnler113@gmail.com>
2022-07-09 11:28:49 +0300

    commit

--- app/src/main/java/com/mertdev/cryptocurrencypricetracker/ui/view/HomeFragment.kt
+++ app/src/main/java/com/mertdev/cryptocurrencypricetracker/ui/view/HomeFragment.kt
@@ -2,23 +2,22 @@ package com.mertdev.cryptocurrencypricetracker.ui.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.mertdev.cryptocurrencypricetracker.R
 import com.mertdev.cryptocurrencypricetracker.databinding.FragmentHomeBinding
-import com.mertdev.cryptocurrencypricetracker.databinding.FragmentRegisterBinding
+import com.mertdev.cryptocurrencypricetracker.ui.viewmodel.HomeFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeFragmentViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
-
 
     }
 
