bf33ebde41c39ea0b7de4087da86ba5b622e6af9
mrtunluer <mertnler113@gmail.com>
2022-07-09 11:27:12 +0300

    commit

--- app/src/main/java/com/mertdev/cryptocurrencypricetracker/ui/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/mertdev/cryptocurrencypricetracker/ui/viewmodel/HomeFragmentViewModel.kt
@@ -2,17 +2,12 @@ package com.mertdev.cryptocurrencypricetracker.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.ExperimentalPagingApi
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.mertdev.cryptocurrencypricetracker.data.model.CoinItem
 import com.mertdev.cryptocurrencypricetracker.data.repo.CoinRepo
-import com.mertdev.cryptocurrencypricetracker.data.repo.FirebaseRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
