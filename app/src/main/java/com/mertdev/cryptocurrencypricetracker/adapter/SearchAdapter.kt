6f1dcd45294d0e6ec1125e95c791d206b0da4b30
mrtunluer <mertnler113@gmail.com>
2022-07-10 07:04:33 +0300

    commit

---
+++ app/src/main/java/com/mertdev/cryptocurrencypricetracker/adapter/SearchAdapter.kt
@@ -0,0 +1,66 @@
+package com.mertdev.cryptocurrencypricetracker.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.mertdev.cryptocurrencypricetracker.data.model.CoinItem
+import com.mertdev.cryptocurrencypricetracker.databinding.CoinsItemBinding
+
+class SearchAdapter: RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
+
+    private val differCallback = object : DiffUtil.ItemCallback<CoinItem>() {
+        override fun areItemsTheSame(oldItem: CoinItem, newItem: CoinItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        @SuppressLint("DiffUtilEquals")
+        override fun areContentsTheSame(oldItem: CoinItem, newItem: CoinItem): Boolean {
+            return oldItem == newItem
+        }
+    }
+    private val differ = AsyncListDiffer(this, differCallback)
+
+    fun submitList(list: List<CoinItem>) {
+        differ.submitList(list)
+    }
+
+    private fun getItem(position: Int): CoinItem {
+        return differ.currentList[position]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            CoinsItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val coin = getItem(position)
+
+        val name = coin.name
+        val symbol = coin.symbol?.uppercase()
+        val price = coin.currentPrice.toString()
+
+        holder.binding.coinText.text = name.plus(" $symbol")
+        holder.binding.priceTxt.text = price
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    inner class ViewHolder(val binding: CoinsItemBinding):
+        RecyclerView.ViewHolder(binding.root)
+
+    private var onItemClickListener: ((CoinItem) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (CoinItem) -> Unit) { onItemClickListener = listener }
+
+}
--- app/src/main/java/com/mertdev/cryptocurrencypricetracker/ui/view/HomeFragment.kt
+++ app/src/main/java/com/mertdev/cryptocurrencypricetracker/ui/view/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.mertdev.cryptocurrencypricetracker.ui.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.core.view.isVisible
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.mertdev.cryptocurrencypricetracker.R
 import com.mertdev.cryptocurrencypricetracker.adapter.CoinsPagingAdapter
 import com.mertdev.cryptocurrencypricetracker.adapter.FooterAdapter
+import com.mertdev.cryptocurrencypricetracker.adapter.SearchAdapter
 import com.mertdev.cryptocurrencypricetracker.data.model.CoinItem
 import com.mertdev.cryptocurrencypricetracker.databinding.FragmentHomeBinding
 import com.mertdev.cryptocurrencypricetracker.ui.viewmodel.HomeFragmentViewModel
@@ -24,6 +27,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var binding: FragmentHomeBinding
     private val viewModel: HomeFragmentViewModel by viewModels()
     private val coinsPagingAdapter = CoinsPagingAdapter()
+    private val searchAdapter = SearchAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,11 +35,65 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         initRv()
         observeCoin()
         loadStateListener()
+        search()
 
         binding.swipeRefreshLayout.setOnRefreshListener {
             viewModel.getCoins()
         }
 
+    }
+
+    private fun search(){
+        binding.searchTxt.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do Nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Do Nothing
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s == null || s.isEmpty()){
+                    showPagingRv()
+                }else if (s.toString().length >= 3){
+                    showSearchRv()
+                    filter(s)
+                }
+            }
+
+        })
+    }
+
+    private fun showSearchRv(){
+        binding.searchRv.visibility = View.VISIBLE
+        binding.pagingRv.visibility = View.GONE
+    }
+
+    private fun showPagingRv(){
+        binding.searchRv.visibility = View.GONE
+        binding.pagingRv.visibility = View.VISIBLE
+    }
+
+    private fun filter(text: Editable?){
+        val filteredList = arrayListOf<CoinItem>()
+        for(item in coinsPagingAdapter.snapshot().toMutableList()){
+            item?.name?.let {
+                if (it.contains(text.toString().lowercase())){
+                    filteredList.add(item)
+                }
+            }
+        }
+        searchAdapter.submitList(filteredList)
+        isResultExist(filteredList.size)
+    }
+
+    private fun isResultExist(size: Int){
+        if (size == 0){
+            binding.noResultTxt.visibility = View.VISIBLE
+        }else{
+            binding.noResultTxt.visibility = View.GONE
+        }
     }
 
     private fun loadStateListener(){
@@ -46,10 +104,15 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     }
 
     private fun initRv(){
-        binding.recyclerView.apply {
+        binding.pagingRv.apply {
             layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
             adapter = coinsPagingAdapter.withLoadStateFooter(FooterAdapter{
                 coinsPagingAdapter.retry() })
+        }
+
+        binding.searchRv.apply {
+            layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
+            adapter = searchAdapter
         }
     }
 
