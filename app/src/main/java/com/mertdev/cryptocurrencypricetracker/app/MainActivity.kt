ddbd0b3ddc11e9607e0ab63babbc8179b9e973c8
mrtunluer <mertnler113@gmail.com>
2022-07-10 03:28:33 +0300

    commit

--- app/src/main/java/com/mertdev/cryptocurrencypricetracker/app/MainActivity.kt
+++ app/src/main/java/com/mertdev/cryptocurrencypricetracker/app/MainActivity.kt
@@ -2,8 +2,13 @@ package com.mertdev.cryptocurrencypricetracker.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.ui.setupWithNavController
+import com.mertdev.cryptocurrencypricetracker.R
 import com.mertdev.cryptocurrencypricetracker.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.navigation.findNavController
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -14,6 +19,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navController = findNavController( R.id.fragmentContainer)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        setupNav()
+
+    }
+
+    private fun setupNav() {
+
+        val navController = findNavController( R.id.fragmentContainer)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.registerFragment -> hideBottomNav()
+                R.id.loginFragment -> hideBottomNav()
+                else -> showBottomNav()
+            }
+        }
+
+    }
+
+    private fun showBottomNav() {
+        binding.bottomNavigationView.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        binding.bottomNavigationView.visibility = View.GONE
     }
 
 }
