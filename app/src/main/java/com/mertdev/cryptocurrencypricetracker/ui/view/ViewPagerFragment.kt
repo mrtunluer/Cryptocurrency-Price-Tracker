f23eb8d55b88985b797a4f11afa3903d0b64c203
mrtunluer <mertnler113@gmail.com>
2022-07-13 13:23:01 +0300

    commit

--- app/src/main/java/com/mertdev/cryptocurrencypricetracker/ui/view/ViewPagerFragment.kt
+++ app/src/main/java/com/mertdev/cryptocurrencypricetracker/ui/view/ViewPagerFragment.kt
@@ -2,13 +2,10 @@ package com.mertdev.cryptocurrencypricetracker.ui.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.google.android.material.tabs.TabLayoutMediator
 import com.mertdev.cryptocurrencypricetracker.R
 import com.mertdev.cryptocurrencypricetracker.adapter.ViewPagerAdapter
-import com.mertdev.cryptocurrencypricetracker.databinding.FragmentFavoriteBinding
 import com.mertdev.cryptocurrencypricetracker.databinding.FragmentViewPagerBinding
 import com.mertdev.cryptocurrencypricetracker.utils.Constants.TABS_FRAGMENT_TEXT
 import dagger.hilt.android.AndroidEntryPoint
