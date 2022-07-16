8543be6dfeabfd26753391832e8b9368e9697254
mrtunluer <mertnler113@gmail.com>
2022-07-16 19:03:32 +0300

    commit

--- app/src/main/java/com/mertdev/cryptocurrencypricetracker/ui/viewmodel/FavoriteFragmentViewModel.kt
+++ app/src/main/java/com/mertdev/cryptocurrencypricetracker/ui/viewmodel/FavoriteFragmentViewModel.kt
@@ -2,27 +2,33 @@ package com.mertdev.cryptocurrencypricetracker.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
-import com.mertdev.cryptocurrencypricetracker.data.datasource.FavoritePagingSource
-import com.mertdev.cryptocurrencypricetracker.utils.Constants.FAVORITE_PAGE_SIZE
-import com.mertdev.cryptocurrencypricetracker.utils.Constants.PREFETCH_DISTANCE
+import com.mertdev.cryptocurrencypricetracker.data.repo.FavoritesRepo
+import com.mertdev.cryptocurrencypricetracker.utils.CoinListState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FavoriteFragmentViewModel @Inject constructor(
-    private val favoritePagingSource: FavoritePagingSource
+    private val favoritesRepo: FavoritesRepo
 ) : ViewModel(){
 
-    val favoriteCoins = Pager(
-        config = PagingConfig(
-            pageSize = FAVORITE_PAGE_SIZE,
-            enablePlaceholders = false,
-            prefetchDistance = PREFETCH_DISTANCE
-        ),
-        pagingSourceFactory = {favoritePagingSource}
-    ).flow.cachedIn(viewModelScope)
+    private val _state = MutableStateFlow(CoinListState())
+    val state: StateFlow<CoinListState> get() = _state
 
+    init {
+        getFavoriteCoins()
+    }
+
+    fun getFavoriteCoins() {
+        viewModelScope.launch {
+            favoritesRepo.getFavoriteCoins().cachedIn(viewModelScope).collectLatest {
+                _state.value = CoinListState(it)
+            }
+        }
+    }
 }
