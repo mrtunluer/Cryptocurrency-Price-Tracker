ee1055c74526d5cce1a530669c731311c48a0eb0
mrtunluer <mertnler113@gmail.com>
2022-07-18 18:21:43 +0300

    commit

--- app/src/main/java/com/mertdev/cryptocurrencypricetracker/data/repo/CoinDetailsRepo.kt
+++ app/src/main/java/com/mertdev/cryptocurrencypricetracker/data/repo/CoinDetailsRepo.kt
@@ -2,15 +2,13 @@ package com.mertdev.cryptocurrencypricetracker.data.repo
 
 import com.mertdev.cryptocurrencypricetracker.data.model.CoinDetails
 import com.mertdev.cryptocurrencypricetracker.service.ApiService
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class CoinDetailsRepo @Inject constructor(private val apiService: ApiService) {
     fun getCoinDetails(id: String): Flow<CoinDetails>
     = flow {
         emit(apiService.getCoinDetails(id))
-    }.flowOn(Dispatchers.IO)
+    }
 }
